6c9f63f87d31a32d30bf1eff30986b1a3db739c9
b1ack0p <blackop@disroot.org>
2022-09-26 21:51:51 +0300

    edit

---
+++ data-structures/variables-demo.py
@@ -0,0 +1,39 @@
+"""
+1- write the followings as variables
+
+Customer First Name
+Customer Last Name
+Customer First Name + Last Name
+Customer ID number
+Customer birth year
+Customer Address details
+Customer Email
+Customer Age
+"""
+
+customer_firstName = 'Martin'
+customer_lastName = 'Smith'
+print(customer_firstName + ' ' + customer_lastName)
+customerID = 123456789
+customerBirth = 1979
+customerAddr = 'N/A'
+customerEmail = "martin@mail.com"
+customerAge = print(2022 - customerBirth)
+
+"""
+2 - calculate sum of the orders below
+
+order 1 = $110
+order 2 = $1200.5
+order 3 = $700.99
+"""
+
+print(110 + 1200.5 + 700.99)    # 2011.49
+
+# or
+
+order1 = 110
+order2 = 1200.5
+order3 = 700.99
+
+print(order1 + order2 + order3) # 2011.49
