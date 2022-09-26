2cd2d216385d013809738d1b8b78bdfcb984a8ce
b1ack0p <blackop@disroot.org>
2022-09-26 19:58:51 +0300

    edit

---
+++ data-structures/math-operators.py
@@ -0,0 +1,17 @@
+# Math Operators
+#----------------------
+#   Symbol  |   Name
+#----------------------
+#   +       |   Sum
+#   -       |   Subtraction
+#   *       |   Multiply
+#   /       |   Divide
+#   **      |   Exponential form (uslu sayilar)
+#   //      |   Full divide (tam bolme)
+
+print(2*0.5)
+#output: 1.0
+print(1/2)
+#output: 0.5
+print(3*5+7-1/4)
+#output:
--- data-structures/numbers.py
+++ data-structures/numbers.py
@@ -1,3 +1,17 @@
 #integers: 3, 5, 8, 1000, 152, -23
 #floating: 3.5, 7,25, 56,1142, 0.0
 
+# 'type()' code shows type of the object
+type(589)
+#output: class 'int' > integer number
+type(0.0012)
+#output: clas 'float' > floating number
+
+print(2*3)
+#output: 6
+print(2/0.56)
+#output: 3.571428571428571
+print(2*7+10-5.1/98)
+#output: 23.94795918367347
+print(2*7+(10-5.1)/98)
+#output: 14.05
